50b16d8e8c393d9999169ae5a1f4d9646c446502
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-08-23 15:11:50 +0300

    pass arguments in the constructor created for accounts

--- accounts.py
+++ accounts.py
@@ -5,4 +5,14 @@ class account:
 
     pass
 
-    def __init__(self,account,user_name,email_aaddress):
+    def __init__(self,account,user_name,email_address,password):
+
+        '''
+        defined properties of objects
+        '''
+
+        Args:
+        account: New account.
+        user_name: New account user name.
+        email_address: New account email email_address.
+        password: New account password.
