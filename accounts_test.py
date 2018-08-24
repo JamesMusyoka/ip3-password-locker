3da7dd42f91e632c8cce222b40c8221ec6aae54f
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-08-24 15:18:40 +0300

    create test for deleting accounts

--- accounts_test.py
+++ accounts_test.py
@@ -11,6 +11,13 @@ class TestAccounts(unittest.TestCase):
         unittest.TestCase: Test case class that helps in creating test cases.
     '''
 
+
+    def tearDown(self):
+        '''
+        This method does clean up after each test has run.
+        '''
+        Accounts.accounts_list = []
+
     def setUp(self):
         '''
         Set up method to run before each test cases.
@@ -45,4 +52,22 @@ if __name__ == '__main__':
         This is to test if we can save multiple objects to our accounts_list.
         '''
         self.new_accounts.save_accounts()
-        test_accounts = Accounts("Test", "user", )
+        test_accounts = Accounts("Test", "user", "nanciekathini@gmail.com", "test@user.com")
+        test_accounts.save_accounts()
+        self.assertEqual(len(Accounts.accounts_list),2)
+
+if __name__ == '__main__':
+    unittest.main()
+
+    def test_delete_accounts(self):
+        '''
+        This is to testif we can remove an account from our accounts list.
+        '''
+        self.new_accounts.save_accounts()
+        test_accounts = Accounts("Test", "user", "nanciekathini@gmail.com")
+        test_accounts.save_accounts()
+
+        self.new_accounts.delete_account()
+        self.assertEqual(len(Accounts.accounts_list),1)
+    if __name__ == '__main__':
+        unittest.main()        
